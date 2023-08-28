f7a5a39c069b204a37f62370e0afff03038b9cdb
wildanfatah22 <awildanfatahillah@gmail.com>
2023-08-28 21:18:19 +0700

    Trying use BG Thread, Loopj, and Retrofit

---
+++ Fundamental Android Apps/RestaurantReview/app/src/main/java/com/example/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,26 @@
+package com.example.restaurantreview.data.retrofit
+
+import com.example.restaurantreview.data.response.PostReviewResponse
+import com.example.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(@Path("id") id: String): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
+}
