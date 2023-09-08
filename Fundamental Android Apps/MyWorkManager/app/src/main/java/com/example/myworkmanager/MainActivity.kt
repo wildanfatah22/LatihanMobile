7425cb32612de7ebbfc6b95d309522bda95665a3
wildanfatah22 <awildanfatahillah@gmail.com>
2023-09-08 23:11:12 +0700

    Latihan WorkManager

---
+++ Fundamental Android Apps/MyWorkManager/app/src/main/java/com/example/myworkmanager/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.myworkmanager
+
+import android.Manifest
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.work.Constraints
+import androidx.work.Data
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.example.myworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var workManager: WorkManager
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
+    private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener(this)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        workManager = WorkManager.getInstance(this)
+
+        binding.btnOneTimeTask.setOnClickListener(this)
+        binding.btnPeriodicTask.setOnClickListener(this)
+        binding.btnCancelTask.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnOneTimeTask -> startOneTimeTask()
+            R.id.btnPeriodicTask -> startPeriodicTask()
+            R.id.btnCancelTask -> cancelPeriodicTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(oneTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this@MainActivity) { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+            }
+    }
+    private fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        periodicWorkRequest =
+            PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build()
+        workManager.enqueue(periodicWorkRequest)
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this@MainActivity) { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+                binding.btnCancelTask.isEnabled = false
+                if (workInfo.state == WorkInfo.State.ENQUEUED) {
+                    binding.btnCancelTask.isEnabled = true
+                }
+            }
+    }
+
+    private fun cancelPeriodicTask() {
+        workManager.cancelWorkById(periodicWorkRequest.id)
+    }
+
+}
